072010fc9205a5e4f5799987019a29a3fdf2f8fe
SGPD\dmuppaneni <divya.gorantla@safeguardproperties.com>
2017-05-04 10:20:35 -0400

    Adding the implementation for database and cache

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -1,6 +1,10 @@
 package cache
 
 import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+
 	"github.com/bradfitz/gomemcache/memcache"
 	"github.com/divyag9/gomodel/pkg/interface"
 	"github.com/divyag9/gomodel/pkg/pb"
@@ -13,7 +17,7 @@ type Config struct {
 	OrderNumberGetter contentserviceinterface.OrderNumberGetter
 }
 
-//NewOrderClient initializes the cache client to get imagedetails for ordernumber
+//NewOrderClient initializes the cache client to get Imagedetails for ordernumber
 func NewOrderClient(memcache *memcache.Client, secondsToExpiry int32, orderNumberGetter contentserviceinterface.OrderNumberGetter) *Config {
 	return &Config{
 		Memcache:          memcache,
@@ -22,17 +26,72 @@ func NewOrderClient(memcache *memcache.Client, secondsToExpiry int32, orderNumbe
 	}
 }
 
-//GetImageDetailsByOrderNumber retrieves the imageDetails for a given orderNumber
+//GetImageDetailsByOrderNumber retrieves the ImageDetails for a given orderNumber
 func (c *Config) GetImageDetailsByOrderNumber(orderNumber int64) ([]*contentservice.ImageDetail, error) {
-	return []*contentservice.ImageDetail{&contentservice.ImageDetail{}}, nil
+	//Construct the key for caching
+	key := fmt.Sprintf("OrderNumber:%q", orderNumber)
+
+	//Get ImageDetails from cache for a given key
+	imageDetails, err := getImageDetails(key, c.Memcache)
+	if err != nil {
+		//Making a call to the database cause the results are not in cache
+		imageDetails, err = c.OrderNumberGetter.GetImageDetailsByOrderNumber(orderNumber)
+		if err != nil {
+			return nil, err
+		}
+
+		//Set ImageDetails retrieved from database to cache
+		err = setImageDetails(key, imageDetails, c)
+		if err != nil {
+			return nil, err
+		}
+
+		return imageDetails, nil
+	}
+
+	return imageDetails, nil
 }
 
-//GetImageDetailsByImageIds retrieves the imageDetails for a given set og imageIds
-func (c *Config) GetImageDetailsByImageIds(imageIds []int64) ([]*contentservice.ImageDetail, error) {
-	return []*contentservice.ImageDetail{&contentservice.ImageDetail{}}, nil
+//getImageDetails retrieves the ImageDetails from cache for a given key
+func getImageDetails(key string, memcacheClient *memcache.Client) ([]*contentservice.ImageDetail, error) {
+	//Retieve value for a key from memcache
+	item, err := memcacheClient.Get(key)
+	if err != nil {
+		return nil, err
+	}
+	imageDetailBytes := item.Value
+
+	//Deserialize ImageDetail bytes to ImageDetail struct
+	imageDetails := []*contentservice.ImageDetail{}
+	decBuf := bytes.NewBuffer(imageDetailBytes)
+	err = gob.NewDecoder(decBuf).Decode(&imageDetails)
+	if err != nil {
+		return nil, err
+	}
+
+	return imageDetails, nil
 }
 
 //setImageDetails sets the ImageDetails to cache for a given key
-func setImageDetails(key string, imageDetails []*contentservice.ImageDetail) error {
+func setImageDetails(key string, imageDetails []*contentservice.ImageDetail, config *Config) error {
+	//Serialize ImageDetails into bytes
+	encBuf := new(bytes.Buffer)
+	err := gob.NewEncoder(encBuf).Encode(imageDetails)
+	if err != nil {
+		return err
+	}
+
+	//Setting ImageDetail bytes to cache
+	err = config.Memcache.Set(&memcache.Item{Key: key, Value: encBuf.Bytes(), Expiration: config.SecondsToExpiry})
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+//GetImageDetailsByImageIds retrieves the ImageDetails for a given set og imageIds
+func (c *Config) GetImageDetailsByImageIds(imageIds []int64) ([]*contentservice.ImageDetail, error) {
+	//To be implemented
+	return []*contentservice.ImageDetail{&contentservice.ImageDetail{}}, nil
 }
--- pkg/database/database.go
+++ pkg/database/database.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"fmt"
+
 	"github.com/divyag9/gomodel/pkg/pb"
 	ora "gopkg.in/rana/ora.v4"
 )
@@ -19,10 +21,32 @@ func NewConfig(session *ora.Ses) *Config {
 
 //GetImageDetailsByOrderNumber retrieves the imageDetails for a given orderNumber
 func (c *Config) GetImageDetailsByOrderNumber(orderNumber int64) ([]*contentservice.ImageDetail, error) {
-	return []*contentservice.ImageDetail{&contentservice.ImageDetail{}}, nil
+	//Prepare the query
+	prepStatement, err := c.Session.Prep("CALL CONTENTSERVICE.RETRIEVEDEPARTMENTS(:1)")
+	if err != nil {
+		return nil, err
+	}
+	defer prepStatement.Close()
+
+	//Retrieve the resultSet
+	resultSet := &ora.Rset{}
+	rownum, err := prepStatement.Exe(resultSet)
+	if err != nil {
+		return nil, err
+	}
+
+	//Trying to print the values retured from ref_cursor
+	fmt.Println(rownum)
+	fmt.Println(resultSet.Row)
+	for _, v := range resultSet.Columns {
+		fmt.Println(v.Name)
+	}
+
+	return []*contentservice.ImageDetail{}, nil
 }
 
 //GetImageDetailsByImageIds retrieves the imageDetails for a given set og imageIds
 func (c *Config) GetImageDetailsByImageIds(imageIds []int64) ([]*contentservice.ImageDetail, error) {
+	//To be implemented
 	return []*contentservice.ImageDetail{&contentservice.ImageDetail{}}, nil
 }
