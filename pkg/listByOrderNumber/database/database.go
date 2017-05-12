3e3bdf3960cdd9d001b117a92b4f272e09267d6b
SGPD\dmuppaneni <divya.gorantla@safeguardproperties.com>
2017-05-12 14:29:19 -0500

    Refactoring code

--- pkg/listByOrderNumber/database/database.go
+++ pkg/listByOrderNumber/database/database.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/divyag9/gomodel/pkg/pb"
 	"github.com/golang/protobuf/ptypes"
+	google_protobuf "github.com/golang/protobuf/ptypes/timestamp"
 	ora "gopkg.in/rana/ora.v4"
 )
 
@@ -61,119 +62,95 @@ func getImageDetailFromResultSet(resultSet *ora.Rset) (*contentservice.ImageDeta
 	for i, v := range resultSet.Columns {
 		switch v.Name {
 		case "ID":
-			value, err := strconv.ParseInt(resultSet.Row[i].(ora.OCINum).String(), 10, 64)
+			value, err := toInt64FromOCINum(resultSet.Row[i])
 			if err != nil {
 				return nil, err
 			}
 			imageDetail.ImageId = value
 		case "CONTRACTORID":
-			if resultSet.Row[i] != nil {
-				value, err := strconv.ParseInt(resultSet.Row[i].(ora.OCINum).String(), 10, 64)
-				if err != nil {
-					return nil, err
-				}
-				imageDetail.ContractorId = value
+			value, err := toInt64FromOCINum(resultSet.Row[i])
+			if err != nil {
+				return nil, err
 			}
+			imageDetail.ContractorId = value
 		case "ORDERNUMBER":
-			if resultSet.Row[i] != nil {
-				value, err := strconv.ParseInt(resultSet.Row[i].(ora.OCINum).String(), 10, 64)
-				if err != nil {
-					return nil, err
-				}
-				imageDetail.OrderNumber = value
+			value, err := toInt64FromOCINum(resultSet.Row[i])
+			if err != nil {
+				return nil, err
 			}
+			imageDetail.OrderNumber = value
 		case "SCANDATE":
-			if resultSet.Row[i] != nil {
-				scanDate, err := ptypes.TimestampProto(resultSet.Row[i].(time.Time))
-				if err != nil {
-					return nil, err
-				}
-				imageDetail.ScanDate = scanDate
+			scanDate, err := toTimestamp(resultSet.Row[i])
+			if err != nil {
+				return nil, err
 			}
+			imageDetail.ScanDate = scanDate
 		case "RELEASEDATE":
-			if resultSet.Row[i] != nil {
-				releaseDate, err := ptypes.TimestampProto(resultSet.Row[i].(time.Time))
-				if err != nil {
-					return nil, err
-				}
-				imageDetail.ReleaseDate = releaseDate
+			releaseDate, err := toTimestamp(resultSet.Row[i])
+			if err != nil {
+				return nil, err
 			}
+			imageDetail.ReleaseDate = releaseDate
 		case "IMAGEFILENAME":
 			if resultSet.Row[i] != nil {
-				imageDetail.ImageFileName = resultSet.Row[i].(string)
+				imageDetail.ImageFileName = toString(resultSet.Row[i])
 			}
 		case "WEBFILENAME":
 			if resultSet.Row[i] != nil {
-				imageDetail.WebFileName = resultSet.Row[i].(string)
+				imageDetail.WebFileName = toString(resultSet.Row[i])
 			}
 		case "IMAGETYPE":
 			if resultSet.Row[i] != nil {
-				imageDetail.ImageType = int32(resultSet.Row[i].(int64))
+				imageDetail.ImageType = toInt32FromNumber(resultSet.Row[i])
 			}
 		case "IMAGEWIDTH":
 			if resultSet.Row[i] != nil {
-				imageDetail.ImageWidth = int32(resultSet.Row[i].(int64))
+				imageDetail.ImageWidth = toInt32FromNumber(resultSet.Row[i])
 			}
 		case "IMAGEHEIGHT":
 			if resultSet.Row[i] != nil {
-				imageDetail.ImageHeight = int32(resultSet.Row[i].(int64))
+				imageDetail.ImageHeight = toInt32FromNumber(resultSet.Row[i])
 			}
 		case "DEPTCODE":
 			if resultSet.Row[i] != nil {
-				imageDetail.DeptCode = resultSet.Row[i].(string)
+				imageDetail.DeptCode = toString(resultSet.Row[i])
 			}
 		case "ARCHIVED":
 			if resultSet.Row[i] != nil {
-				imageDetail.Archived = resultSet.Row[i].(string)
+				imageDetail.Archived = toString(resultSet.Row[i])
 			}
 		case "FILESIZE":
-			if resultSet.Row[i] != nil {
-				value, err := strconv.ParseInt(resultSet.Row[i].(ora.OCINum).String(), 10, 32)
-				if err != nil {
-					return nil, err
-				}
-				imageDetail.FileSize = int32(value)
+			value, err := toInt32FromOCINum(resultSet.Row[i])
+			if err != nil {
+				return nil, err
 			}
+			imageDetail.FileSize = value
 		case "THUMBNAILSIZE":
-			if resultSet.Row[i] != nil {
-				value, err := strconv.ParseInt(resultSet.Row[i].(ora.OCINum).String(), 10, 32)
-				if err != nil {
-					return nil, err
-				}
-				imageDetail.ThumbnailSize = int32(value)
+			value, err := toInt32FromOCINum(resultSet.Row[i])
+			if err != nil {
+				return nil, err
 			}
+			imageDetail.ThumbnailSize = value
 		case "DATECREATED":
-			if resultSet.Row[i] != nil {
-				dateCreated, err := ptypes.TimestampProto(resultSet.Row[i].(time.Time))
-				if err != nil {
-					return nil, err
-				}
-				imageDetail.DateCreated = dateCreated
+			dateCreated, err := toTimestamp(resultSet.Row[i])
+			if err != nil {
+				return nil, err
 			}
+			imageDetail.DateCreated = dateCreated
 		case "DATEMODIFIED":
-			if resultSet.Row[i] != nil {
-				dateModefied, err := ptypes.TimestampProto(resultSet.Row[i].(time.Time))
-				if err != nil {
-					return nil, err
-				}
-				imageDetail.DateModified = dateModefied
+			dateModefied, err := toTimestamp(resultSet.Row[i])
+			if err != nil {
+				return nil, err
 			}
+			imageDetail.DateModified = dateModefied
 		case "DESCPREFIX":
-			if resultSet.Row[i] != nil {
-				imageDetail.DescPrefix = resultSet.Row[i].(string)
-			}
+			imageDetail.DescPrefix = toString(resultSet.Row[i])
 		case "DESCTEXT":
-			if resultSet.Row[i] != nil {
-				imageDetail.DescText = resultSet.Row[i].(string)
-			}
+			imageDetail.DescText = toString(resultSet.Row[i])
 		case "CATEGORY":
-			if resultSet.Row[i] != nil {
-				imageDetail.Category = resultSet.Row[i].(string)
-			}
+			imageDetail.Category = toString(resultSet.Row[i])
 		case "GUID":
-			if resultSet.Row[i] != nil {
-				imageDetail.Guid = resultSet.Row[i].(string)
-			}
+			imageDetail.Guid = toString(resultSet.Row[i])
 		}
 	}
 	//Generate these
@@ -181,4 +158,65 @@ func getImageDetailFromResultSet(resultSet *ora.Rset) (*contentservice.ImageDeta
 	//imageDetail.GeneratedImageFilePath,
 	//imagerotated
 	return imageDetail, nil
+}
+
+//Convert resultset value of columns that are of type string
+func toString(columnValue interface{}) string {
+	var result string
+	if columnValue != nil {
+		result = columnValue.(string)
+	}
+
+	return result
+}
+
+//Convert resultset value of columns that are of type Date
+func toTimestamp(columnValue interface{}) (*google_protobuf.Timestamp, error) {
+	var result *google_protobuf.Timestamp
+	if columnValue != nil {
+		timestampValue, err := ptypes.TimestampProto(columnValue.(time.Time))
+		if err != nil {
+			return nil, err
+		}
+		result = timestampValue
+	}
+	return result, nil
+}
+
+//Convert resultset value of columns that are of type OCINum
+func toInt64FromOCINum(columnValue interface{}) (int64, error) {
+	var result int64
+	if columnValue != nil {
+		parseValue, err := strconv.ParseInt(columnValue.(ora.OCINum).String(), 10, 64)
+		if err != nil {
+			return 0, err
+		}
+		result = parseValue
+	}
+
+	return result, nil
+}
+
+//Convert resultset value of columns that are of type OCINum
+func toInt32FromOCINum(columnValue interface{}) (int32, error) {
+	var result int32
+	if columnValue != nil {
+		parseValue, err := strconv.ParseInt(columnValue.(ora.OCINum).String(), 10, 32)
+		if err != nil {
+			return 0, err
+		}
+		result = int32(parseValue)
+	}
+
+	return result, nil
+}
+
+//Convert resultset value of columns that are of type Number
+func toInt32FromNumber(columnValue interface{}) int32 {
+	var result int32
+	if columnValue != nil {
+		result = int32(columnValue.(int64))
+	}
+
+	return result
 }
